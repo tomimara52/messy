a37195a928359588d89e0ab92e139803dbb6ffb6
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-02-08 01:04:18 -0300

    create multiple threads for each tcp connection
    also fix bug in send_messages function
    and now the server doesn't resend you your message

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::net::{SocketAddr, TcpListener};
 use std::io::{BufRead, BufReader};
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 use crate::Client;
 
@@ -7,68 +9,86 @@ use crate::Client;
 
 pub struct Server {
     listener: TcpListener,
-    clients: Vec<Client>
+    clients: Arc<Mutex<Vec<Client>>>
 }
 
 impl Server {
     pub fn new(addr: &str) -> Server {
         let listener = TcpListener::bind(addr).unwrap();
 
-        Server{ listener, clients: vec![] }
+        Server{ listener, clients: Arc::new(Mutex::new(vec![])) }
     }
 
     pub fn listen(&mut self) {
-        let (stream, addr) = self.listener.accept().unwrap();
+        loop {
+            let (stream, addr) = self.listener.accept().unwrap();
+            let clients = Arc::clone(&self.clients);
 
-        let buf_reader = BufReader::new(stream.try_clone().unwrap());
+            thread::spawn(move || {
+                let buf_reader = BufReader::new(stream.try_clone().unwrap());
 
-        let requests = buf_reader
-            .lines()
-            .map(|l| l.unwrap());
+                let requests = buf_reader
+                    .lines()
+                    .map(|l| l.unwrap());
 
-        for request in requests {
-            println!("Received: {:#?}", request);
+                for request in requests {
+                    println!("Received: {:#?}", request);
 
-            if request.starts_with("CONNECT ") {
-                let nick = request.split(' ').nth(1).unwrap();
-                let stream = stream.try_clone().unwrap();
+                    if request.starts_with("CONNECT ") {
+                        let nick = request.split(' ').nth(1).unwrap();
+                        let stream = stream.try_clone().unwrap();
 
-                self.clients.push(
-                    Client::from_stream(nick, stream));
+                        clients.lock().unwrap().push(
+                            Client::from_stream(nick, stream));
 
-                println!("{nick} connected.");
-            } else if request.starts_with("SEND ") {
-                let msg = &request[5..];
+                        println!("{nick} connected.");
+                    } else if request.starts_with("SEND ") {
+                        let msg = &request[5..];
 
-                self.send_messages(addr, msg);
-            }
-        }
-    }
-
-    fn send_messages(&mut self, sender_addr: SocketAddr, msg: &str) {
-        let sender_nick = self.clients
-            .iter()
-            .take_while(|c| c.peer_addr().unwrap() == sender_addr)
-            .last()
-            .unwrap()
-            .nick();
-
-        let msg = String::from("SENT ") +
-            &String::from(sender_nick) +
-            " " + msg + "\n";
-
-        let mut to_delete = vec![];
-
-        for (i, client) in self.clients.iter_mut().enumerate() {
-            if let Err(e) = client.write_stream(msg.as_bytes()) {
-                println!("Error: {e}");
-                to_delete.push(i);
-            }
-        }
-
-        for i in to_delete {
-            self.clients.remove(i);
+                        send_messages(&mut clients.lock().unwrap(), addr, msg);
+                    }
+                }
+            });
         }
     }
 
 }
+
+fn send_messages(
+    clients: &mut Vec<Client>,
+    sender_addr: SocketAddr,
+    msg: &str
+) {
+    let same_address = |c: &Client| c.peer_addr().unwrap() == sender_addr;
+    let sender_index = match clients.iter().position(same_address) {
+            None => {
+                println!("Unrecognized client");
+                return;
+            },
+            Some(i) => i
+    };
+    
+    let sender_nick = clients[sender_index].nick();
+
+    let msg = String::from("SENT ") +
+        &String::from(sender_nick) +
+        " " + msg + "\n";
+
+    let mut to_delete = vec![];
+
+    for (i, client) in clients.iter_mut().enumerate() {
+        if same_address(client) {
+            continue;
+        }
+
+        if let Err(e) = client.write_stream(msg.as_bytes()) {
+            println!("Error: {e}");
+            to_delete.push(i);
+        }
+    }
+
+    for i in to_delete {
+        clients.remove(i);
+    }
+}
+
