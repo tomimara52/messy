39145b4dd50a846138a9ec35e08b1036c77ebb8e
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-02-21 19:29:06 -0300

    do not lose what is being written when a message arrives
    lost ability to delete characters lol

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,8 @@
-use std::io::{self, stdin, BufRead, BufReader, Write};
+use std::io::{self, stdin, stdout, BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpStream};
 use std::thread;
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use std::sync::mpsc::{self, Receiver, Sender};
 
 #[derive(Debug)]
 pub struct Client {
@@ -28,8 +30,9 @@ impl Client {
     pub fn connect(&mut self) {
         let request = String::from("CONNECT ") + &self.nick + "\n";
         self.stream.write(request.as_bytes()).unwrap();
-        self.listen();
-        self.input_loop();
+        let (tx, rx) = mpsc::channel();
+        self.listen(rx);
+        self.input_loop(tx);
     }
 
     pub fn nick(&self) -> &String {
@@ -44,18 +47,39 @@ impl Client {
         self.addr
     }
 
-    fn input_loop(&mut self) {
+    fn input_loop(&mut self, tx: Sender<String>) {
         let mut message = String::new();
 
         loop {
             message.clear();
+            tx.send(String::new()).unwrap();
 
-            print!("{}> ", self.nick);
-            io::stdout().flush().unwrap();
+            print!("\r\n{}> ", self.nick);
+            stdout().flush().unwrap();
 
-            stdin().read_line(&mut message).unwrap();
+            let mut buf: [u8; 1] = [0];
+            enable_raw_mode().unwrap();
 
-            if message.trim().starts_with("!quit") {
+            loop { 
+                stdin().read(&mut buf).unwrap();
+
+                if buf[0] == 10 || buf[0] == 13 {
+                    break;
+                }
+
+                message.push(buf[0] as char);
+
+                tx.send(message.clone()).unwrap();
+
+                write!(stdout(), "{}", buf[0] as char).unwrap();
+                stdout().flush().unwrap();
+            }
+
+            disable_raw_mode().unwrap();
+
+            message.push_str("\r\n");
+
+            if message.starts_with("!quit") {
                 message.replace_range(
                     0..5,
                     "DISCONNECT"
@@ -70,11 +94,14 @@ impl Client {
             self.stream.write(request.as_bytes()).unwrap();
         }
 
+        println!("");
+
     }
 
-    fn listen(&mut self) {
+    fn listen(&mut self, rx: Receiver<String>) {
         let stream = self.stream.try_clone().unwrap();
         let client_nick = self.nick.clone();
+        let mut message = String::new();
 
         thread::spawn(move || loop {
             let buf_reader = BufReader::new(&stream);
@@ -87,20 +114,20 @@ impl Client {
                     let nick = request_words.nth(1).unwrap();
                     let msg = &request[4+nick.len()+2..];
 
-                    println!("\r\x1b[K{nick}: {msg}");
+                    writeln(format!("\r\x1b[K{nick}: {msg}"));
                     print!("{}> ", client_nick);
-                    io::stdout().flush().unwrap();
+                    stdout().flush().unwrap();
                 } else if request.starts_with("INTRODUCE ") {
                     let nick = match request.split(' ').nth(1) {
                         Some(s) => s,
                         None => continue
                     };
 
-                    println!("\r\x1b[K{nick} connected to the server.");
+                    writeln(format!("\r\x1b[K{nick} connected to the server."));
 
                     if nick != client_nick {
                         print!("{}> ", client_nick);
-                        io::stdout().flush().unwrap();
+                        stdout().flush().unwrap();
                     }
                 } else if request.starts_with("GOODBYE ") {
                     let mut request_words = request.split(' ');
@@ -110,21 +137,37 @@ impl Client {
                         None => continue
                     };
 
-                    match request_words.next() {
+                    let msg = match request_words.next() {
                         Some(s) if s != "" => {
-                            println!("\r\x1b[K{nick} disconnected with message: {s}.");
+                            format!("\r\x1b[K{nick} disconnected with message: {s}.")
                         },
                         None|Some(_) => { 
-                            println!("\r\x1b[K{nick} disconnected from the server.");
+                            format!("\r\x1b[K{nick} disconnected from the server.")
                         }
                     };
 
+                    writeln(msg);
+
                     print!("{}> ", client_nick);
-                    io::stdout().flush().unwrap();
+                    stdout().flush().unwrap();
                 }
+
+                loop {
+                    match rx.try_recv() {
+                        Ok(s) => message = s,
+                        Err(_) => break
+                    }
+                };
+
+                print!("{}", message);
+                stdout().flush().unwrap();
             }
         });
 
         thread::sleep(std::time::Duration::from_millis(250));
     }
+}
+
+fn writeln(s: String) {
+    write!(stdout(), "{s}\r\n").unwrap();
 }
