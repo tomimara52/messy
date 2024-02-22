2bb9c5450965f805a48f456f6308bfa7eeecb6a2
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-02-21 23:39:39 -0300

    error handle

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,8 @@
+use std::error::Error;
 use std::io::{self, stdin, stdout, BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpStream};
 use std::thread;
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode, is_raw_mode_enabled};
 use std::sync::mpsc::{self, Receiver, Sender};
 
 #[derive(Debug)]
@@ -32,7 +33,15 @@ impl Client {
         self.stream.write(request.as_bytes()).unwrap();
         let (tx, rx) = mpsc::channel();
         self.listen(rx);
-        self.input_loop(tx);
+
+        if let Err(e) = self.input_loop(tx) {
+            if is_raw_mode_enabled().expect("Error checking if raw mode is enabled.") {
+                disable_raw_mode().expect("Error disabling raw mode.");
+            }
+
+            println!("Input loop failed with: {e}.");
+            std::process::exit(1);
+        }
     }
 
     pub fn nick(&self) -> &String {
@@ -47,15 +56,15 @@ impl Client {
         self.addr
     }
 
-    fn input_loop(&mut self, tx: Sender<String>) {
+    fn input_loop(&mut self, tx: Sender<String>) -> Result<(), Box<dyn Error>> {
         let mut message = String::new();
 
         'outer: loop {
             let mut buf: [u8; 1] = [0];
-            enable_raw_mode().unwrap();
+            enable_raw_mode()?;
 
             loop { 
-                stdin().read(&mut buf).unwrap();
+                stdin().read(&mut buf)?;
 
                 // handle pressing ENTER
                 if buf[0] == 10 || buf[0] == 13 {
@@ -67,28 +76,28 @@ impl Client {
                     if !message.is_empty() {
                         message.pop();
                         let b = 8 as char;
-                        write!(stdout(), "{b} {b}").unwrap();
-                        stdout().flush().unwrap();
+                        write!(stdout(), "{b} {b}")?;
+                        stdout().flush()?;
                     }
                     continue;
                 }
 
                 // handle pressing CTRL-C
                 if buf[0] == 3 {
-                    disable_raw_mode().unwrap();
+                    disable_raw_mode()?;
                     break 'outer;
                 }
 
                 // handle rest of characters
                 message.push(buf[0] as char);
 
-                tx.send(message.clone()).unwrap();
+                tx.send(message.clone())?;
 
-                write!(stdout(), "{}", buf[0] as char).unwrap();
-                stdout().flush().unwrap();
+                write!(stdout(), "{}", buf[0] as char)?;
+                stdout().flush()?;
             }
 
-            disable_raw_mode().unwrap();
+            disable_raw_mode()?;
 
             message.push_str("\r\n");
 
@@ -98,23 +107,23 @@ impl Client {
                     "DISCONNECT"
                 );
 
-                self.stream.write(message.as_bytes()).unwrap();
+                self.stream.write(message.as_bytes())?;
                 break;
             }
 
             let request = String::from("SEND ") + &message;
 
-            self.stream.write(request.as_bytes()).unwrap();
+            self.stream.write(request.as_bytes())?;
 
             message.clear();
-            tx.send(String::new()).unwrap();
+            tx.send(String::new())?;
 
             print!("\r\n{}> ", self.nick);
-            stdout().flush().unwrap();
+            stdout().flush()?;
         }
 
         println!("");
-
+        Ok(())
     }
 
     fn listen(&mut self, rx: Receiver<String>) {
